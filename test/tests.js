738d1dba2718859fcea71fccbf5f6a78f60f5629
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2017-06-21 12:23:55 -0300

    refatoração dos testes em andamentos

--- test/tests.js
+++ test/tests.js
@@ -7,11 +7,12 @@ const
   chai = require('chai'),
   chaiHttp = require('chai-http'),
   should = chai.should(),
+  _ = require("underscore"),
   expect = chai.expect;
 
 const //o app
   server = require('../src/server');
-  
+
 
 const //modelos
   Service = require("../src/models/serviceModel"),
@@ -29,14 +30,9 @@ const //constantes
   API_REQUESTS_BASE_URL = '/api/v1/requests',
   API_NOTIFICATIONS_BASE_URL = '/api/v1/notifications';
 
-var //objetos usados nos testes
-  validRequest = {};
-var
-  validService = {};
-
-/** Funções-promessas para tarefas comuns */
+/** Funções-promessa para tarefas comuns */
 const getRequestsList = () => {
-  return new Promise((resolve, reject)=>{
+  return new Promise((resolve, reject) => {
     chai.request(server)
       .get(`${API_BASE_URL}/requests`)
       .end((err, res) => {
@@ -46,7 +42,7 @@ const getRequestsList = () => {
 }
 
 const insertValidRequest = () => {
-  return new Promise((resolve, reject)=>{
+  return new Promise((resolve, reject) => {
     chai
       .request(server)
       .post(`${API_BASE_URL}/requests`)
@@ -58,60 +54,229 @@ const insertValidRequest = () => {
 }
 
 
-
-function clearCollections() {
+/**
+ * @function Limpa as coleções
+ */
+const clearCollections = () => {
   const modelsToClear = [
-    Service.remove().then(()=>{
-      console.log('Serviços excluidos');
-    }),
-    Request.remove().then(()=>{
-      console.log('Requisições excluidos');
-    }),
+    Service.remove(),
+    Request.remove(),
   ];
   let result = Promise.all(modelsToClear);
   return result;
 }
 
+const _customizeModel = (modelProperties, model, ModelClass) => {
+  return _.extend(model, modelProperties);
+}
 
-function insertMockObjects() {
-  const generateMockObjects = [
-    mockObjects.getValidService(),
-    mockObjects.getValidRequest()
-  ]
 
-  return Promise.all(generateMockObjects)
-    .then((objects)=>{
-      let generatedService = objects[0];
-      let generatedRequest = objects[1];
-      let newService = new Service(generatedService);
-      return newService
-        .save()
-        .then((newService)=>{
-          let newRequest = generatedRequest;
-          validService = newService.toJSON();
-          newRequest.serviceId = newService.id;
-          newRequest = new Request(newRequest);
-          return newRequest.save();
-        })
-        .then((newRequest)=>{
-          validRequest = newRequest.toObject();
-        })
+const insertMockService = (properties = null) => {
+  return mockObjects.getValidService()
+    .then((generatedService) => {
+      _customizeModel(properties, generatedService, Service);
+      return new Service(generatedService).save();
     })
-    .catch((err)=>{
+    .then((newService) => {
+      return newService.toJSON();
+    })
+    .catch((err) => {
+      console.log('falha ao inserir um serviço mock');
+      expect.fail();
+    });
+}
+
+const insertMockRequest = (properties = null, serviceId) => {
+  if (!serviceId) {
+    throw new Error('É necessário fornecer um serviceId para a Requisição');
+  }
+  return mockObjects.getValidRequest()
+    .then((generatedRequest) => {
+      _customizeModel(properties, generatedRequest, Request);
+      generatedRequest.serviceId = serviceId;
+      return new Request(generatedRequest).save();
+    })
+    .then((newRequest) => {
+      return newRequest.toJSON();
+    })
+    .catch((err) => {
+      console.log('falha ao inserir uma requisição mock');
+      expect.fail();
+    });
+}
+
+
+/**
+ * Insere objetos gerados no banco de dados de teste
+ */
+const insertMockObjects = (serviceProperties = null, requestProperties = null) => {
+  let objectsInserted = [];
+  return insertMockService(serviceProperties)
+    .then((newService) => {
+      objectsInserted.push(newService);
+      return insertMockRequest(requestProperties, newService._id);
+    })
+    .then((newRequest) => {
+      objectsInserted.push(newRequest);
+      return objectsInserted;
+    })
+    .catch((err) => {
       console.log('falha ao inserir os objetos mock, saindo.')
       expect.fail();
     });
 }
 
 
-/* Limpe o banco de dados antes de iniciar os testes */
-before(() => {
-  return clearCollections()
-    .then(insertMockObjects);
+
+
+beforeEach(() => {
+  return clearCollections();
 });
 
-describe('Testes com os...',()=>{
-  it('should pass', (done)=>{
-    done();
-  })
+describe('Teste com serviços', () => {
+  /* antes de cada suite, limpe o banco de dados */
+  /*beforeEach(() => {
+    console.log("limpando...");
+    return clearCollections();
+  });*/
+
+  describe('Listagem', () => {
+    it('sem serviços, deve retornar uma lista vazia', (done) => {
+      chai.request(server)
+        .get(API_SERVICES_BASE_URL)
+        .end((err, res) => {
+          res.should.have.status(200);
+          let services = JSON.parse(res.text).data;
+          services.should.be.a('array');
+          services.length.should.eql(0);
+          done();
+        });
+    });
+
+    it('depois de inserir um serviço, deve retornar uma lista com 1 serviço', (done) => {
+      insertMockService({
+          published: true
+        })
+        .then(() => {
+          chai.request(server)
+            .get(API_SERVICES_BASE_URL)
+            .end((err, res) => {
+              res.should.have.status(200);
+              let services = JSON.parse(res.text).data;
+              services.should.be.a('array');
+              services.length.should.eql(1);
+              done();
+            });
+        });
+    });
+  });
+
+  describe('Inserção', () => {
+    it('serviço vazio, deve retornar um 400', (done) => {
+      chai.request(server)
+        .post(API_SERVICES_BASE_URL)
+        .send({})
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        })
+    });
+
+    it('inválido, deve retornar um 400', (done) => {
+      chai.request(server)
+        .post(API_SERVICES_BASE_URL)
+        .send(mockObjects.getInvalidService())
+        .end((err, res) => {
+          res.should.have.status(400);
+          let validationInfo = JSON.parse(res.text).data.body;
+          validationInfo.should.be.a('array');
+
+          let expectedProperties = [
+            'request.body.machine_name',
+            'request.body.name',
+            'request.body.createdAt',
+            'request.body.published'
+          ];
+
+          validationInfo.forEach((property, propertyIndex, properties) => {
+            let removeIndex = expectedProperties.indexOf(properties[propertyIndex].property);
+            if (removeIndex > -1) {
+              expectedProperties.splice(removeIndex, 1);
+            }
+          });
+
+          expectedProperties.length.should.eql(0);
+
+          validationInfo[4].should.be.an("object");
+          validationInfo[4].should.have.property("property", "request.body");
+          validationInfo[4].should.have.property("messages");
+          validationInfo[4].messages.length.should.gt(0);
+          done();
+        });
+    });
+  });
+
+  describe('Atualização', () => {
+    beforeEach(() => {
+      return clearCollections();
+    });
+
+    it('com propriedades permitidas, deve atualizar', (done) => {
+      insertMockService()
+        .then((validService) => {
+          chai.request(server)
+            .put(`${API_SERVICES_BASE_URL}/${validService.machine_name}`)
+            .send({
+              'sa_category': 'Banco de dados.Manutenção'
+            })
+            .end((err, res) => {
+              res.should.have.status(200);
+              let updatedService = res.body.data;
+              updatedService.should.be.a('object');
+              updatedService.should.have.property('sa_category', 'Banco de dados.Manutenção');
+              Date.parse(updatedService.updatedAt).should.be.gt(validService.updatedAt.getTime());
+              done();
+            });
+        });
+    });
+
+    it('com apenas propriedades não permitidas, deve retornar um 400', (done) => {
+      insertMockService()
+        .then((validService) => {
+          chai.request(server)
+            .put(`${API_SERVICES_BASE_URL}/${validService.machine_name}`)
+            .send({
+              'machine_name': 'abacate'
+            })
+            .end((err, res) => {
+              res.should.have.status(400);
+              done();
+            });
+        });
+    });
+
+    it('em caso misto, deve ignorar quem não é atualizável', (done) => {
+      insertMockService()
+        .then((validService) => {
+          chai.request(server)
+            .put(`${API_SERVICES_BASE_URL}/${validService.machine_name}`)
+            .send({
+              'machine_name': 'abacate',
+              'sa_category': 'Manutenção.Banco de dados'
+            })
+            .end((err, res) => {
+              res.should.have.status(200);
+              let updatedService = res.body.data;
+              updatedService.should.have.property('sa_category', 'Manutenção.Banco de dados');
+              updatedService.should.not.have.property('machine_name', 'abacate')
+
+              Date.parse(updatedService.updatedAt).should.be.gt(validService.updatedAt.getTime());
+
+              done();
+            })
+        });
+    });
+  });
+
+
 });
